f7d3ff9f89d34c2983b4ba84d95b59f163326a85
wchang <weipanchang@hotmail.com>
2014-02-04 11:46:31 -0800

    change 30 to 30.0 to make float

--- Clock+Angle.py
+++ Clock+Angle.py
@@ -2,7 +2,12 @@
 
 hour = 3
 minute = 20
-hour_finger = float(hour * (360 /12))
-minutes_finger = float(minute * (360 /60))
-print hour_finger, minutes_finger
+print float(float(minute) * (30.0/60))
+hour_finger = float(hour) * (360 /12) + (float(minute) * float(30 / 60))
+minutes_finger = float(minute) * float(360 /60)
+angle_diff = hour_finger - minutes_finger
+
+
+print hour_finger, minutes_finger, abs(angle_diff)
+
 
