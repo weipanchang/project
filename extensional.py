a6363b5699d3319e3bea0094fc0786d8e68101c3
weipanchang <weipanchang@hotmail.com>
2014-03-06 07:24:49 -0800

    add add_range, remove_range, complement

--- extensional.py
+++ extensional.py
@@ -1,34 +1,65 @@
 #!/usr/bin/env python
 import unittest
 
-def empty_set():
-    return (lambda x: False)
+def empty_set():  return (lambda x: False)
 
-def singleton(x):
-    return (lambda y: y ==x)
-def ranger(a, b):
-    return (lambda x: x > a and x < b)
+def singleton(x):   return (lambda y: y == x)
 
+def range_set(a, b):   return (lambda x: x > a and x < b)
 
-class TestUM(unittest.TestCase):
- 
+def add(s, x): return (lambda y: s(y) or y == x)
+
+def union(s1, s2): return (lambda x: s1(x) or s2(x))
+
+def intersection(s1, s2): return (lambda x: s1(x) and s2(x))
+
+def add_many(s, xs):
+    return (lambda y: (s(y) or y in xs))
+
+def add_range(s, l, u):
+    return (lambda y: (s(y) or (l < y and y < u)))
+
+def remove_range(s, l, u):
+    return (lambda y: s(y) and (y < l or y > u))
+
+def complement(s):
+    return (lambda y: not s(y))
+
+class TestRun(unittest.TestCase):
     def setUp(self):
-        pass
- 
-    def test_empty_set(self):
-        s = empty_set()
-        self.assertEqual(s(5), False)
+        self.a = empty_set()
+        self.b = singleton(3)
+        self.c = range_set(2, 6)
+        self.d = range_set(4, 10)
+         
+    def test_add_many(self):
+        s = add_many(self.c, [6, 7, 8, 9])
+        self.assertEqual(s(3), True)
+        self.assertEqual(s(8), True)
+        self.assertEqual(s(10), False)
+        self.assertEqual(s(2), False)
+    
+    def test_add_range(self):
+        s = add_range(self.c, 6, 9)
+        self.assertEqual(s(3), True)
+        self.assertEqual(s(8), True)
+        self.assertEqual(s(10), False)
+        self.assertEqual(s(2), False)        
 
-    def singleton(self):
-        s = singleton(5)
+    def test_remove_range(self):
+        s = remove_range(self.d, 7, 9)
         self.assertEqual(s(5), True)
-        self.assertEqual(s(2), False)
+        self.assertEqual(s(8), False)
+        self.assertEqual(s(6), True)
+        self.assertEqual(s(10), False)
         
-    def test_empty_set(self):
-        s = ranger(4,10)
-        self.assertEqual(s(5), True)
-        self.assertEqual(s(2), False)
-        self.assertEqual(s(12), False)
+    def test_complement(self):
+        s = complement(self.c)
+        self.assertEqual(s(3), False)
+        self.assertEqual(s(8), True)
+        self.assertEqual(s(6), True)
+        self.assertEqual(s(10), True) 
+
     def teardown(self):
         pass
  
