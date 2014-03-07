c7cda8abaa3bf2c9637ec687405a345e94acc951
weipanchang <weipanchang@hotmail.com>
2014-03-06 17:23:51 -0800

    map and filter module

--- extensional.py
+++ extensional.py
@@ -19,11 +19,30 @@ def add_many(s, xs):
 def add_range(s, l, u):
     return (lambda y: (s(y) or (l < y and y < u)))
 
+def remove(s, x):
+    return (lambda y: s(y) and (not y== x))
+
+def remove_many(s, xs):
+    return (lambda y: s(y) and (not y in xs))
+
 def remove_range(s, l, u):
     return (lambda y: s(y) and (y < l or y > u))
 
 def complement(s):
     return (lambda y: not s(y))
+
+def add_one(s):
+    return (lambda y: s(y-1))
+
+def map_set(f, s):
+    t = f(s)
+    return (lambda y: t(y))
+
+def even(y):
+    return  True if ((y % 2) == 0) else False
+
+def filter_set(p, s):
+    return (lambda y: p(y) and s(y))
 
 class TestRun(unittest.TestCase):
     def setUp(self):
@@ -31,20 +50,55 @@ class TestRun(unittest.TestCase):
         self.b = singleton(3)
         self.c = range_set(2, 6)
         self.d = range_set(4, 10)
-         
+
+    def test_empty_set(self):
+        s = empty_set()
+        self.assertEqual(s(5), False)
+
+    def test_singleton(self):
+        s = singleton(5)
+        self.assertEqual(s(5), True)
+        self.assertEqual(s(2), False)
+
+    def test_range_set(self):
+        s = range_set(4,10)
+        self.assertEqual(s(4), False)
+        self.assertEqual(s(5), True)
+        self.assertEqual(s(10), False)
+
+    def test_add(self):
+        s = add(self.c, 8)
+        self.assertEqual(s(8), True)
+        self.assertEqual(s(3), True)
+        self.assertEqual(s(6), False)
+
     def test_add_many(self):
         s = add_many(self.c, [6, 7, 8, 9])
         self.assertEqual(s(3), True)
         self.assertEqual(s(8), True)
         self.assertEqual(s(10), False)
         self.assertEqual(s(2), False)
-    
+
     def test_add_range(self):
         s = add_range(self.c, 6, 9)
         self.assertEqual(s(3), True)
         self.assertEqual(s(8), True)
         self.assertEqual(s(10), False)
-        self.assertEqual(s(2), False)        
+        self.assertEqual(s(2), False)
+
+    def test_remove(self):
+        s = remove(self.d, 7)
+        self.assertEqual(s(5), True)
+        self.assertEqual(s(7), False)
+        self.assertEqual(s(6), True)
+        self.assertEqual(s(10), False)
+
+    def test_remove_many(self):
+        s = remove_many(self.d, [5,6])
+        self.assertEqual(s(5), False)
+        self.assertEqual(s(7), True)
+        self.assertEqual(s(6), False)
+        self.assertEqual(s(10), False)
 
     def test_remove_range(self):
         s = remove_range(self.d, 7, 9)
@@ -52,16 +106,77 @@ class TestRun(unittest.TestCase):
         self.assertEqual(s(8), False)
         self.assertEqual(s(6), True)
         self.assertEqual(s(10), False)
-        
+
+    def test_union(self):
+        s = union(self.c, self.d)
+        self.assertEqual(s(3), True)
+        self.assertEqual(s(2), False)
+        self.assertEqual(s(8), True)
+        self.assertEqual(s(10), False)
+
+    def test_intersection(self):
+        s = intersection(self.c, self.d)
+        self.assertEqual(s(3), False)
+        self.assertEqual(s(5), True)
+        self.assertEqual(s(6), False)
+        self.assertEqual(s(8), False)
+
     def test_complement(self):
         s = complement(self.c)
         self.assertEqual(s(3), False)
         self.assertEqual(s(8), True)
         self.assertEqual(s(6), True)
-        self.assertEqual(s(10), True) 
-
+        self.assertEqual(s(10), True)
+        
+    def test_add_one(self):
+        s = empty_set()
+        t = add_one(s)
+        self.assertEqual(t(6), False)
+        self.assertEqual(t(5), False)
+        
+        s = singleton(5)
+        t = add_one(s)
+        self.assertEqual(t(6), True)
+        self.assertEqual(t(5), False)
+        
+        s = range_set(3, 9)
+        t = add_one(s)
+        self.assertEqual(t(3), False)
+        self.assertEqual(t(4), False)
+        self.assertEqual(t(5), True)
+        self.assertEqual(t(8), True)
+        self.assertEqual(t(9), True)
+        self.assertEqual(t(10), False)
+        
+    def test_map_set(self):
+        s = range_set(3, 9)
+        t = map_set(add_one, s)
+        self.assertEqual(t(3), False)
+        self.assertEqual(t(4), False)
+        self.assertEqual(t(5), True)
+        self.assertEqual(t(6), True)
+        self.assertEqual(t(7), True)
+        self.assertEqual(t(9), True)
+        self.assertEqual(t(10), False)
+    
+    def test_even(self):
+        self.assertEqual(even(3), False)
+        self.assertEqual(even(4), True)
+        self.assertEqual(even(5), False)
+        
+    def test_filter_set(self):
+        s = range_set(3, 12)
+        t = filter_set(even, s)
+        self.assertEqual(t(3), False)
+        self.assertEqual(t(4), True)
+        self.assertEqual(t(5), False)
+        self.assertEqual(t(6), True)
+        self.assertEqual(t(7), False)
+        self.assertEqual(t(10), True)
+        self.assertEqual(t(12), False)
+    
     def teardown(self):
         pass
- 
+
 if __name__ == '__main__':
     unittest.main()
