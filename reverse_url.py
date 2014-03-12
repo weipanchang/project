7f9c485c393f5808bf870d7196866d1bc9cdb410
weipanchang <weipanchang@hotmail.com>
2014-03-12 10:12:21 -0700

    modify reverse_url.py

--- reverse_url.py
+++ reverse_url.py
@@ -3,14 +3,16 @@
 def reverse_label(s):
     tokens = s.split(".")
     
-    prefix = tokens[0]
-    suffix = "".join(tokens[2:])
-    center = tokens[1]
+    #prefix = tokens[0]
+    #suffix = "".join(tokens[2:])
+    #center = tokens[1]
+    new = '.'.join(tokens[::-1])
     
-    return suffix + '.' + center + '.' + prefix
+    #return suffix + '.' + center + '.' + prefix
+    return new
 
 def do_list(l):
     for i in l:
         print reverse_label(i)
         
-do_list([www.nominum.com], [www2.sfo.example.com])
+do_list(['www.nominum.com', 'www2.sfo.example.com'])
