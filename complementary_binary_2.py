983e556643eeb8030f6b7efe6831240c8f747db9
weipanchang <weipanchang@hotmail.com>
2014-03-11 21:49:07 -0700

    more recursive on complementary_binaruy_2.py

--- complementary_binary_2.py
+++ complementary_binary_2.py
@@ -13,34 +13,24 @@ def decimal_to_binary(n):
                 x = '1'
             else: x = '0'
             return x + (recursive(n, i - 1))
-    
-    #m = int(math.log(n,2))
+
     return recursive(n, int(math.log(n,2)))
     
-    #return l
-     
-    #binary_string = ''   
-    #l = []   
-    #for i in range(m,-1,-1):
-    #    if n >= int(math.pow(2, i)):
-    #        l.append('1')
-    #        n = n - int(math.pow(2, i))
-    #    else:
-    #        l.append('0')
-    #
-    #binary_string = ''.join(l)
-    #return binary_string
 
 def complementary(s):
     return ''.join(['1' if c == '0' else '0' for c in s] )
 
 def binary_to_decimal(s):
-    result = 0
-    for i, j in enumerate(s[::-1]):
-        result += math.pow(2,i) * int(j)
-    return int(result)
+# s is visble under recursive
+    def recursive( i):
+        if i == len(s) - 1:
+            return int(s[i]) * int(math.pow(2, len(s)- i -1))
+        else:
+            result = int(s[i]) * int(math.pow(2, len(s)- i - 1))
+            return result + recursive(  i + 1)
+    return recursive(0)
 
-print decimal_to_binary(16)
+#print decimal_to_binary(16)
 #print complementary('1101')
-#print binary_to_decimal('0010')
-print binary_to_decimal(complementary(decimal_to_binary(16)))
+#print binary_to_decimal('0001')
+print binary_to_decimal(complementary(decimal_to_binary(50)))
